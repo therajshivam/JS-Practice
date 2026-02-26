b06ac5da110800a753a9022686157c50baa984c8
Shivam Raj <rajshivam5984@gmail.com>
2026-02-27 00:29:01 +0530

    array methods

---
+++ JSforCypress/04arraysFunctions/lect4_arraysMethodsQues.js
@@ -0,0 +1,9 @@
+// ARRAY METHODS QUESTIONS
+
+let companies = ["bloomsberg", "microsoft", "uber", "google", "IBM", "netflix"]
+companies.splice(0, 1)
+console.log(companies);
+companies.shift()
+console.log(companies);
+companies.push("amazon")
+console.log(companies);
