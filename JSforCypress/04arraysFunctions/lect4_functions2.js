2a6d8addbeeee4e4463a054c3dc5e1cfd51476e0
Shivam Raj <rajshivam5984@gmail.com>
2026-02-28 21:00:45 +0530

    functions

--- JSforCypress/04arraysFunctions/lect4_functions2.js
+++ JSforCypress/04arraysFunctions/lect4_functions2.js
@@ -4,3 +4,35 @@
 // CallBackFunction : here, it is a function to execute for each element in the array
 // a callback is a function passed as an argument to another function
 
+let arr = [1, 2, 3, 4, 5];
+arr.forEach(function printValue(val) {
+  // val = value at each index
+  console.log(val);
+});
+
+let arrCities = ["mumbai", "delhi", "chandigarh", "pune"];
+arrCities.forEach((val, index, arrCities) => {
+  // console.log(val);
+  console.log(val.toUpperCase(), index, arrCities);
+});
+
+// forEach have 3 parameters -> value, index and array
+
+// HIGHER ORDER FUNCTION / higher order method
+// can take another function as a parameter or returns another function as value
+// forEach() is a higher order function
+
+// Ques : For a given array of numbers, print the square of each value using forEach loop.
+
+let nums = [1, 2, 3, 4, 5, 6];
+
+// nums.forEach((sqNums)=>{
+//     console.log(sqNums**2);
+// })
+
+// another way
+let calcSquare = (sqNums) => {
+  console.log(sqNums ** 2);
+};
+
+nums.forEach(calcSquare);
---
+++ JSforCypress/04arraysFunctions/lect4_functions3.js
@@ -0,0 +1,20 @@
+// some more array methods
+
+// 1. map
+// creates a new array with the results of some operation. The value its callback returns are used to form new array.
+// arr.map(callbackFnx(value, index, array))
+
+let nums = [67, 52, 38]
+
+// print
+nums.map((val) => {
+    console.log(val)
+})
+
+// make new array
+let newArr = nums.map((val)=>{
+    // return val
+    return val ** 2;
+})
+console.log(newArr);
+
