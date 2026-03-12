6d8a6cc0c73259848f772724f6556bf7392b5a9a
Shivam Raj <rajshivam5984@gmail.com>
2026-03-12 23:55:18 +0530

    ARRAY

--- JSforCypress/05ArraysAndObjects/02arraysFunctions.js
+++ JSforCypress/05ArraysAndObjects/02arraysFunctions.js
@@ -22,4 +22,6 @@ const realAnotherArray = anotherArray.flat(Infinity)
 console.log(realAnotherArray);
 
 // Duing web scraping data may come in another format like nodelist, object or string but we want it in array
-console.log(Array.isArray("hello")) // checks if the give is array or not
+console.log(Array.isArray("hello")) // checks if the give is array or not 
+
+ 
