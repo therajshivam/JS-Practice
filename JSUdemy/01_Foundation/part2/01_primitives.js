bfcdc06a7f6022060cec07450118a4a642678cd0
Shivam Raj <rajshivam5984@gmail.com>
2026-06-20 15:28:43 +0530

    update

---
+++ JSUdemy/01_Foundation/part2/01_primitives.js
@@ -0,0 +1,13 @@
+/*
+DataTypes - primitives and non-primitives
+1. Primitives - number, string, boolean, bigint, undefined, null, symbol
+2. Non-primitives - objects (objects, arrays, functions)
+*/
+
+// Number 
+let balance = 120
+console.log(balance);
+console.log(typeof(balance));
+
+let anotherBalance = new Number(120)
+console.log(anotherBalance.valueOf());
