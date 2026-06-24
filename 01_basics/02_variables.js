6946415f130aba60a1ba2bdfcc7667b261d692ca
Shivam Raj <rajshivam5984@gmail.com>
2026-06-24 11:08:53 +0530

    variables

--- 01_basics/02_variables.js
+++ 01_basics/02_variables.js
@@ -3,7 +3,7 @@ const accountId = 542156 // use to declare constant, constant cant be change
 let accountEmail = "abc@gmail.com" // use to declare variable
 var accountPassword = "12345"
 accountCity = "Jaipur" //do not use
-let accountState; // undefined 
+let accountState; // declaration; output is undefined 
 
 // accountId = 2 // not allowed
 
@@ -20,3 +20,14 @@ because of issue in block scope and in functional scope
 */
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// practice
+const uid = '20BCS'
+const name = 'Shivam Raj'
+let section = 612
+var block = 'A1'
+university = 'CU'
+let department
+console.log(uid)
+console.table([uid, name, section, block, university, department])
