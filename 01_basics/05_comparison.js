60ef5122df08d210da2370a907aa27b912eed4aa
Shivam Raj <rajshivam5984@gmail.com>
2026-06-24 20:30:08 +0530

    comparisons

--- 01_basics/05_comparison.js
+++ 01_basics/05_comparison.js
@@ -1,18 +1,31 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1); // Basic conversions; no problem; both sides have numbers
+// number comparison
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1); // Basic comparisons; no problem; both sides have numbers
 
-// console.log("2" > 1);
-// console.log("02" > 1); // inconsistency in lang; avoid these conversions
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0); // inconsistency in lang; avoid these conversions
+// not same datatype comparison
+console.log("2" > 1);
+console.log("02" > 1); // inconsistency in lang; avoid these conversions
 
-// console.log(undefined > 0); // inconsistency in lang; avoid these conversions
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+// The reason is that the equality check == and comparisons > < >= <= work differently.
+// Comparisons convert null to a number, treating it as a 0.
+// Thats why (3) null >= 0 is true but (1) null > 0 is false. 
+// inconsistency in lang; avoid these conversions
+
+console.log(undefined > 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+// undefined will give false whatever the comparison or equality check.
+// inconsistency in lang; avoid these conversions
+
+// in js, comparison operators (< > <= >=) and equality operator (==) behaves differently from each other.
 
 // === => strict check
 // it checks the values and its data types with conversion
-console.log("2" === 2);
+console.log("2" === 2); // false
