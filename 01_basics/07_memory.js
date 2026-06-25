281b041bd81d4f721cefc2a49a77988607022c39
Shivam Raj <rajshivam5984@gmail.com>
2026-06-25 09:09:04 +0530

    memory

--- 01_basics/07_memory.js
+++ 01_basics/07_memory.js
@@ -1,24 +1,32 @@
-// Two types of momory : Stack (primitive) and Heap (non-primitive)
+// Two types of memory : Stack (primitive) and Heap (non-primitive)
 
-// STACK gives copy value
+// STACK : STACK stores primitive values. Copying creates a new value.
+// - Used for Primitive Data Types.
+// - Stores the actual value.
+// - Faster access.
+// - When copied, a new copy of the value is created.
 
-let myYoutubeName = "onedotcom"
-let anotherName = "two"
+let myYoutubeName = "hiteshchaudhary";
+let anotherName = myYoutubeName;
+anotherName = "chaiaurcode";
 
-console.log(myYoutubeName);
+console.log(myYoutubeName); // The original value remains unchanged.
 console.log(anotherName);
 
-// HEAP gives reference value; changes will be affect original value
+
+// HEAP : HEAP stores objects. Variables store references (addresses) to those objects. Copying shares the same reference, so changes affect the original object.
+// - Used for Non-Primitive Data Types.
+// - The actual object is stored in the Heap.
+// - Variables store a reference (address) to that object.
+// - When copied, the reference is copied, not the object.
 
 let userOne = {
     email : "user@google.com",
     upi : "user@ybl"
 }
+let userTwo = userOne;
+userTwo.email = "two@google.com";
 
-let userTwo = userOne
-
-userTwo.email = "two@google.com"
-
-console.log(userOne.email);
-console.log(userTwo.email);
+console.log(userOne.email); // Both variables point to the same object.
+console.log(userTwo.email); // Both variables point to the same object.
 
