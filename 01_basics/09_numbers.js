c612062b67fa0617b018b87c0a1f96c815dfef6e
Shivam Raj <rajshivam5984@gmail.com>
2026-06-28 15:46:58 +0530

    numbers and maths

--- 01_basics/09_numbers.js
+++ 01_basics/09_numbers.js
@@ -1,17 +1,25 @@
+// Numbers
+
 const score = 400
-console.log(score);
+console.log(score, typeof score); // number
 
 const balance = new Number(100)
-console.log(balance);
+console.log(balance, typeof balance); // object (exclusively declaring number)
 
 // methods
-console.log(balance.toString()); // converts to string
+console.log(balance.toString(), typeof balance); // converts to string
 console.log(balance.toString().length); // counts length of string
 console.log(balance.toFixed(2)); // precision value
 
 const otherNumber = 123.5966
 console.log(otherNumber.toPrecision(3)); // precised value on number given (round of value on 3rd num)
+// return an string
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN')); // puts commas in the num; default US system, 'en-IN' - indian num sys
 
+// maxvalue and minvalue
+// Number.MIN_VALUE
+// Number.MAX_VALUE
+// Number.MIN_SAFE_INTEGER
+// Number.MAX_SAFE_INTEGER
--- 01_basics/10_maths.js
+++ 01_basics/10_maths.js
@@ -1,25 +1,30 @@
-  console.log(Math);
+console.log(Math); // Object [Math] {}
 
-console.log(Math.abs(-4)); // converts neg num to positive
-console.log(Math.round(4.6)); // round off the given num
+console.log(Math.abs(-4)); // absolute value, converts negative values to positive
+
+// round off the given num
+console.log(Math.round(4.3)); // 4 
+console.log(Math.round(4.6)); // 5
 console.log(Math.ceil(4.2)); // round off the number with upper value
 console.log(Math.floor(4.9)); // round off the number with lower value
+
+console.log(Math.sqrt(49)); // square root
 
 console.log(Math.min(3, 5 , 9 , 7 , 2)); // finds lowest/minimum value
 console.log(Math.max(3, 5 , 9 , 7 , 2)); // finds highest/maximum value
 
+// mostly usecase of math library
 console.log(Math.random()); // always gives values between 0 and 1; can be in decimals
 console.log((Math.random()*10) + 1);  // shifts value by 1 and avoids if answer is 0
 console.log(Math.floor(Math.random()*10) + 1); // gives num without decimals; value lies between 1 and 9
 
 // to get random value between given min and max value
-
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random()*(max-min+1)) + min) // values between 10 and 20
 // Math.floor => without decimals
 // Math.random() => gives random value
 // max - min => gives value in the range
 // + 1 => avoids 0 case
-//  + min => to get min value
+// + min => value starts from min value
