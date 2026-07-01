0041d3d6e7c3c106cf4d10b211dee999625c4a77
Shivam Raj <rajshivam5984@gmail.com>
2026-07-01 10:48:50 +0530

    objects

--- 02_basics/05_objects.js
+++ 02_basics/05_objects.js
@@ -1,21 +1,18 @@
 // destructuring of objects
-
 const course = {
     courseName: "JS in Hindi",
     price: "999",
     courseInstructor: "shivam"
 }
-
-// course.courseInstructor 
-
+// course.courseInstructor -- its good but can be use below method for writing clean code as to not repeat same things
 const {courseInstructor} = course
 console.log(courseInstructor);
-
+// or manually giving variable name
 const {courseInstructor: instructor} = course
 console.log(instructor);
+// will be use in react
 
 // JSON API 
-
 // {
 //     "name": "abc",
 //     "coursename": "js in hindi",
@@ -24,5 +21,5 @@ console.log(instructor);
 
 // Json is used to store apis values
 // it is like in object formate
-// diff is objects have variable name
-// json key is also string formate
+// difference is objects have variable name
+// json key is also in string formate
