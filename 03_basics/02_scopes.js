65df7d41a28becf362f5026095106cf2b80d64ff
Shivam Raj <rajshivam5984@gmail.com>
2026-07-01 23:51:22 +0530

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,10 @@
-// {} => scopes; comes with function or if-else; not with object bcz that is called object declaration
+// {} can represent either:
+// 1. A block scope (if, for, while, function body)
+// 2. An object literal
+// It depends on the context.
 
-// global scope: outside the scope, can be access inside the function
-// block/local scope: inside the scope, cant
+// Global scope: Variables declared outside any block or function. They can be accessed from anywhere.
+// Block scope: Variables declared with let and const inside {}, are only accessible inside that block.
 
 if (true) {
     let a = 10
@@ -11,6 +14,9 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c); --> var leaks out of the block scope
+
+// var is function-scoped, not block-scoped.
+// Avoid using var in modern JavaScript.
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 let a = 300
@@ -23,6 +29,8 @@ if (true) {
 
 console.log("Global: ", a);
 
+// Variable Shadowing: A local variable with the same name hides the outer variable.
+
 // global scope in node and browser are different.
 // inside browser is called core scope.
 
@@ -31,10 +39,12 @@ console.log("Global: ", a);
 // SCOPE LEVEL
 
 // NESTED SCOPE IN FUNCTION
+// Inner scope can access outer scope.
+// Outer scope cannot access inner scope.
 
 // inner "two" nested scope items can take values from "one" scope but the "one" cant take from inner 
 
-// CLOSURE
+// Closure: An inner function remembers and can access variables from its outer function, even after the outer function finishes.
 
 function one(){
     const username = "abc"
@@ -67,7 +77,7 @@ if (true) {
 
 // +++++++++++++++++++++++ INTERESTING ++++++++++++++++++++++++++++++++++
 // HOISTING => how function is declared, stored and executed and how variables are treated 
-// declaring functions
+// JavaScript moves declarations to the top of their scope before executing the code.
 
 addOne(5) // can be access before declaration of the function
 function addOne(num) {
@@ -75,7 +85,14 @@ function addOne(num) {
 }
 
 // addTwo(5) // hoisting, cannot access before declaration of the function
-// addTwo => function expression
+// addTwo => function expression 
 const addTwo = function(num){  // declared in variable
     return num + 2
 }
+
+// Function expressions are NOT hoisted like function declarations.
+
+
+// Scope Chain:
+// JavaScript looks for a variable in the current scope first.
+// If not found, it checks the parent scope.
