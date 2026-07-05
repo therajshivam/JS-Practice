e39c937d9cf2f09352bca7a4a0d3e095c9484474
Shivam Raj <rajshivam5984@gmail.com>
2026-07-05 12:45:50 +0530

    for-loop

--- 05_iterations/01_for.js
+++ 05_iterations/01_for.js
@@ -1,64 +1,81 @@
 // for loop
 
-// for (let i = 0; i <= 10; i++) {
-//     const element = i;
-//     if (element == 5) {
+// counting till 10 -- with a condition 
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
         // console.log("5 is the best number");
-//     }
+    }
     // console.log(element);
-    
-// }
+}
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// for (let i = 0; i <=10; i++) {
-//     console.log(`Outer loop value: ${i}`);
-//     for (let j = 0; j <= 10; j++) {
-//         console.log(`Inner loop value: ${j} and inner loop ${i}`);
-        
-//     }
-// }
+// nested loop
+for (let i = 0; i <=10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+    }
+}
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// for (let i = 1; i <=10; i++) {
-//     console.log(`Outer loop value: ${i}`);
-//     for (let j = 1; j <= 10; j++) {
-//         console.log(i + ' * ' + j + ' = ' + i*j);
-        
-//     }
-// }
+// table printing
+for (let i = 1; i <=10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(i + ' * ' + j + ' = ' + i*j);
+        // console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Array
+// for-loop and Array
 
-// let myArray = ["flash", "batman", "superman"]
+let myArray = ["flash", "batman", "superman"]
 // console.log(myArray.length);
 
-// for (let index = 0; index < myArray.length; index++) {
-//     const element = myArray[index];
-//     console.log(element);
-// }
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    // console.log(element);
+}
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // break and continue
 
-// for (let index = 1; index <= 20; index++) {
-//     if (index == 5) {
-//         console.log("detected 5");
-//         break
-//     }
-//     console.log(`value of i is: ${index}`);
-    
-// }
-
-
-
+// break immediately exits the loop.
 for (let index = 1; index <= 20; index++) {
     if (index == 5) {
-        console.log("detected 5");
+        // console.log("detected 5");
+        break;
+    }
+    // console.log(`value of i is: ${index}`);
+}
+
+// continue skips the current iteration and moves to the next one.
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        // console.log("detected 5");
         continue
     }
-    console.log(`value of i is: ${index}`);
-    
+    // console.log(`value of i is: ${index}`);
 }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// patttern printing
+for (let i = 0; i <5; i++) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+        row += "*";
+    }
+//    console.log(row)
+}
+// OR
+for (let i = 1; i <= 5; i++) {
+    // console.log("*".repeat(i));
+}
+
+// Outer loop → Number of rows
+// Inner loop → Number of stars
