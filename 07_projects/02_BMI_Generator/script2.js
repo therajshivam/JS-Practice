3c8d5e4ff8f85d9fd73cb3657998fa0de4f8fde9
Shivam Raj <rajshivam5984@gmail.com>
2026-07-13 11:40:16 +0530

    dom project-2

--- 07_projects/02_BMI_Generator/script2.js
+++ 07_projects/02_BMI_Generator/script2.js
@@ -8,29 +8,27 @@ form.addEventListener("submit", function (e) {
   const results = document.querySelector("#results");
 
   if (height < 0 || isNaN(height)) {
-     return results.innerHTML = `Enter valid height`;
+    return (results.innerHTML = `Enter valid height`);
   } else if (weight < 0 || isNaN(weight)) {
-     return results.innerHTML = `Enter valid weight`;
+    return (results.innerHTML = `Enter valid weight`);
   } else {
     const bmi = parseFloat((weight / ((height * height) / 10000)).toFixed(2));
     results.innerHTML = `<span>${bmi}</span>`;
-    
+
     function addMsg(value) {
-    const msg = document.createElement("p");
-    msg.appendChild(
-      document.createTextNode(`According to your BMI, You are ${value}`),
-    );
-    results.appendChild(msg);
-  }
+      const msg = document.createElement("p");
+      msg.appendChild(
+        document.createTextNode(`According to your BMI, You are ${value}`),
+      );
+      results.appendChild(msg);
+    }
 
-  if (bmi < 18.6) {
-    addMsg("Underweight");
-  } else if (bmi >= 18.6 && bmi <= 24.9) {
-    addMsg("Normal");
-  } else if (bmi > 24.9) {
-    addMsg("Overweight");
+    if (bmi < 18.6) {
+      addMsg("Underweight");
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+      addMsg("Normal");
+    } else if (bmi > 24.9) {
+      addMsg("Overweight");
+    }
   }
-  }
-
-  
 });
