74709dcc5928f11c88c5d9e699b4c331cf3380b4
Shivam Raj <rajshivam5984@gmail.com>
2026-07-14 19:38:23 +0530

    async project-1

--- 09_asyncProjects/01_unlimitedColors/script2.js
+++ 09_asyncProjects/01_unlimitedColors/script2.js
@@ -1,36 +1,39 @@
 // random color generate
 
 const randomColor = function () {
-    const hex = "0123456789ABCDEF";
-    let color = '#';
-    for (let i = 0; i < 6 ; i++) {
-        
-        // random value genrate
-        color += hex[Math.floor(Math.random() * 16)]
-        
-    }
-    return color;
-}
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    // random value genrate
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const p = document.createElement("p");
+document.body.appendChild(p);
 
 // selectors
-const startButton = document.querySelector('#start')
-const stopButton = document.querySelector('#stop')
+const startButton = document.querySelector("#start");
+const stopButton = document.querySelector("#stop");
 
 // functions
 let intervalId;
-const startChangingColor = function(){
-    function changeBgColor() {
-        document.body.style.backgroundColor = randomColor()
-    }
-    if (!intervalId) {
-        intervalId = setInterval(changeBgColor, 1000)
-    }
-}
-const stopChangingColor = function(){
-    clearInterval(intervalId);
-    intervalId = null;
-}
+const startChangingColor = function () {
+  function changeBgColor() {
+    const colors = randomColor();
+    document.body.style.backgroundColor = colors;
+    p.textContent = `Hex Code : ${colors}`;
+  }
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
 
 // button events
-startButton.addEventListener('click', startChangingColor)
-stopButton.addEventListener('click', stopChangingColor)
+startButton.addEventListener("click", startChangingColor);
+stopButton.addEventListener("click", stopChangingColor);
