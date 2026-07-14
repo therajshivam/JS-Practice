95a2f6091d126e3f5fd51d0b7911f7adf06899b7
Shivam Raj <rajshivam5984@gmail.com>
2026-07-15 01:00:56 +0530

    promises

--- 10_advance_one/02_promises.js
+++ 10_advance_one/02_promises.js
@@ -1,138 +1,152 @@
-// creating promises
+// Promise
+// the promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
 
-// promise is an object
-// new is used to create a new instance
-// promises take callback in it , it also reduce the callback hell
+// Promise is an object.
 
-// promises have two parts : resolve and reject
+// 3 states of promises :
+//     1. pending
+//     2. fulfilled
+//     3. rejected
 
+// promises are : created and consumed.
 
-const promiseOne = new Promise(function(resolve, reject){
-    // Do an async task
-    // DB Calls, cryptography, network
-    setTimeout(function(){
-        console.log('Async task is complete');
-        resolve()  // connects with .then
-    }, 1000)
-    
-})
+// Creating a promise :
+// "new" keyword is used to create a new instance.
+// Promises takes a callback inside it and reduce callback hell.
+// Promise callback function have two parts : resolve and reject.
 
-// consuming promises
-// .then have direct relations with resolve
+const promiseOne = new Promise(function (resolve, reject) {
+  // Do async task
+  // DB calls, cryptography, network calls
 
-promiseOne.then(function(){
-    // returns the value from above 
-    console.log("Promise consumed");    
-})
-
-// always "async task is comsumed will come  " ; because first the task is completed then the task is resolved then the "promise consumed " part will come
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// without storing in an function
-
-new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
-    }, 1000)
-
-}).then(function(){
-    console.log("Async 2 resolved");
-})
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        // we can pass data in resolve parameters 
-        resolve({username: "Chai", email: "chai@example.com"})
-    })
-}) 
-
-promiseThree.then(function(user){
-    console.log(user);
-})
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-const promiseFour = new Promise (function(resolve, reject){
-    setTimeout(function(){
-        let error = false
-        if (!error) {
-            resolve({username: "hitesh", password: "123"})
-        } else{
-            reject('ERROR: SOETHING WENT WRONG')
-        }
-    }, 1000)
-})
-
-// avoiding callback hell
-promiseFour
-.then((user) => {
-    console.log(user);
-    return user.username
-})
-.then((username) => {
-    console.log(username);
-})
-.catch(function(error){
-    console.log(error);
-})
-.finally(() => console.log("the promise is either resolved or rejected"))
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-const promiseFive = new Promise(function (resolve, reject) {
-    setTimeout(function(){
-        let error = true
-        if (!error) {
-            resolve({username: "javascript", password: "123"})
-        } else{
-            reject('ERROR: JS WENT WRONG')
-        }
-    }, 1000)
+  setTimeout(function () {
+    console.log("Async task is complete");
+    resolve(); // connects with .then()
+  }, 1000);
 });
 
-async function consumePromiseFive () {
-    // const response = await promiseFive 
-    // console.log(response);    
-    
-    try {
-        const response = await promiseFive 
-        console.log(response); 
-    } catch (error) {
-        console.log(error);
+// Consuming a promise :
+// .then() have direct relation with the "resolve".
+// .then() have a callback in it, this function automatically recieves argument from the promise creation and returns in the promise consumption.
+
+promiseOne.then(function () {
+  console.log("Promise consumed");
+});
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// without storing in a variable
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Passing values in resolve()
+// data can be passed in resolve() parameters
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "chai", email: "chai@example.com" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Passing error in promise
+// error based checking : suppose accessing file - if file access then resolve , if error not access then reject
+// Reject() gives error
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "chai", password: "123" });
+    } else {
+      reject("ERROR! Something went wrong.");
     }
+  }, 2000);
+});
+
+// .then() : return the values.
+// .catch() : return the error.
+
+// avoiding Callback Hell
+// imp for DB connection
+// .then() chaining : value returned from the first .then() will come into the next .then()
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => console.log("The promise is either resolved or rejected"));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// handling promise using async-await
+// async-await works like .then() & .catch(), waits sometimes till the work is done, if work is completed then it will move else it will give error.
+// async-await cant handle errors directly, for this we use try & catch.
+const promiseFive = new Promise((resolve, reject) => {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "javascript", password: "123" });
+    } else {
+      reject("ERROR! JS went wrong.");
+    }
+  }, 2000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 }
+consumePromiseFive();
 
-consumePromiseFive()
+// .then() & .catch() or async-await use any syntax to consume promises.
 
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// activity : using an real api
+// fetch() will return a promise
+
 // try-catch syntax
-
-// async function getAllUsers(){
-//     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
-//         const data = await response.json() // .jason also takes time to get converted; therefore we have to wait using await
-//         console.log(data);
-//     } catch (error) {
-//         console.log("E: ", error);
-//     }
-// }
-
-// getAllUsers()
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// .then .catch
-
-fetch('https://api.github.com/users/hiteshchoudhary')
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
+async function getAllUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = response.json();
+    // json parsing takes time to convert, hence wait using await
     console.log(data);
-})
-.catch((error) => console.log(error))
+  } catch (error) {
+    console.log(error);
+  }
+}
+getAllUsers();
 
+// .then() & .catch() syntax
+fetch("https://api.github.com/users/therajshivam")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
 
+// github data comes before the earlier promises
