8f38c2750fb3f70ad454bfa80074ba0058ff4a82
Shivam Raj <rajshivam5984@gmail.com>
2026-07-16 17:24:19 +0530

    new keyword internals and prototype

---
+++ proto.js
@@ -0,0 +1,12 @@
+// Understanding prototype:
+
+/*
+Challenge : to create a custom property 
+
+let myName = "hitesh     "
+let channel = "chai     "
+console.log(myName.truelength)
+
+Create a property name truelength which will trim all the empty space and prints the length of the string.
+*/
+
