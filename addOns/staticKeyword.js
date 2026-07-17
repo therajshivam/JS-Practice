697f6d40a8c6e03ed62032c14b6954c853693a22
Shivam Raj <rajshivam5984@gmail.com>
2026-07-17 12:07:12 +0530

    static keyword

--- addOns/staticKeyword.js
+++ addOns/staticKeyword.js
@@ -1,6 +1,7 @@
 // static keyword
 // it defines properties or methods that belong to a class itself rather than the objects created from that class (class owns anything static, not the objects) 
 
+
 class MathUtil {
     static PI = 3.14159;
 
@@ -23,5 +24,34 @@ console.log(MathUtil.getArea(10))
 
 // example 2 : mix of regular properties and static properties
 class User{
-    
+
+    static userCount = 0;
+
+    constructor(username) {
+        this.username = username;
+        User.userCount++;
+    }
+
+    static getUserCount () {
+        console.log(`There are ${User.userCount} users online`);
+        
+    }
+
+    sayHello(){
+        console.log(`My username is ${this.username}`);
+    }
 }
+
+const user1 = new User("Spongebob");
+const user2 = new User("Spongebob");
+const user3 = new User("Sandy");
+
+user1.sayHello()
+user2.sayHello()
+user3.sayHello()
+User.getUserCount()
+
+
+
+console.log(User.userCount);
+
