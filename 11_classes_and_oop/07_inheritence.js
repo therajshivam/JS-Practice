cbf8bb49ed92b5c14881d939fb8c9fec7ced20e5
Shivam Raj <rajshivam5984@gmail.com>
2026-07-17 17:42:50 +0530

    inheritance

--- 11_classes_and_oop/07_inheritence.js
+++ 11_classes_and_oop/07_inheritence.js
@@ -1,33 +1,39 @@
-class User {
-    constructor(username) {
+// inheritance
+
+class User{
+    constructor(username){
         this.username = username
     }
 
     logMe(){
-        console.log(`USERNAME IS ${this.username}`);
+        console.log(`username is ${this.username}`);
     }
 }
 
 class Teacher extends User {
     constructor(username, email, password){
+        // super keyword
         super(username)
         this.email = email
         this.password = password
     }
 
-    addCourse(){
+    addCourses(){
         console.log(`A new course was added by ${this.username}`);
     }
 }
 
-const chai = new Teacher("chai", "chai@teacher.com", "123")
-
-chai.addCourse()
+const chai = new Teacher("Chai", "chai@gmail.com", "123")
+chai.addCourses()
 chai.logMe()
 
-const masalaChai = new User("masalaChai")
+const tea = new User("Tea")
+// tea.addCourses()
+// tea dont have access to addCourses() method 
+tea.logMe()
 
-masalaChai.logMe()
+console.log(chai === tea); // false
+console.log(chai === Teacher); // false
 
-console.log(chai === User);
-console.log(chai instanceof User);
+console.log(chai instanceof Teacher); // true
+console.log(chai instanceof User); // true
